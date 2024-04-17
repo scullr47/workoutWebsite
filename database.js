70086063d0ef8ea430fcc2ba97174ba648dd2df0
scullr47 <167370840+scullr47@users.noreply.github.com>
2024-04-17 19:31:54 -0400

    Update database.js
    Haven't figured out much. Connects to database, not sure if collections are where they need to be.

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://scullr47:RowanMMAFan17!@workoutdb.nc3uu06.mongodb.net/?retryWrites=true&w=majority&appName=WorkoutDB";
-const dbName = "workoutWebsite";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -9,21 +8,32 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
+function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    client.db().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    const db = client.db();
-    const userCol = db.collection('users');
-    const exerciseCol = db.collection('exercises');
-    const workoutCol = db.collection('workouts');
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
-run().catch(console.dir);
+run();
+const db = client.db();
+let users = db.collection('users');
+const exercises = db.collection('exercises');
+const workouts = db.collection('workouts');
 
+async function insertDoc() {
+    try{
+        //await client.connect();
+        users.insertOne({"username" : "insertTest", "password": "12358"});
+        console.log("Added a document");
+    } finally{
+        await client.close();
+    }
+}
+
+console.log(users.findOne());
